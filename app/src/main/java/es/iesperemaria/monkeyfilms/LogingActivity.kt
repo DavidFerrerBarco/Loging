d269aef3d4102bc1ccdc1b14409fe0835c068617
David Ferrer <pizca777@gmail.com>
2022-10-12 16:17:44 +0200

    Initial commit

---
+++ app/src/main/java/es/iesperemaria/monkeyfilms/LogingActivity.kt
@@ -0,0 +1,29 @@
+package es.iesperemaria.monkeyfilms
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import es.iesperemaria.monkeyfilms.databinding.ActivityLogingBinding
+
+class LogingActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityLogingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLogingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+
+
+}
